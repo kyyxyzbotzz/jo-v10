eace4e89e1a34ae086be14ad385fa102303302d3
kyyxyzbotzz <119144784+kyyxyzbotzz@users.noreply.github.com>
2022-12-11 12:06:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'KYYXYZ BOTZZ'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
+global . pemilik  = [ '6281527870349' , '6281527870349' , '6281527870349' ]
+global . premium  = [ '6281527870349' ]
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
